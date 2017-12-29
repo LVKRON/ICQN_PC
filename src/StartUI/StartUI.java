a96529a7f213e0eff039367843d689d02551883c
LVKRON <34261242+LVKRON@users.noreply.github.com>
2017-12-29 21:45:18 +0800

    2017.12.19

--- src/StartUI/StartUI.java
+++ src/StartUI/StartUI.java
@@ -1,17 +1,72 @@
 package StartUI;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class StartUI extends JFrame {
 
+	private JTextField userID = new JTextField();
+	private JTextField userPsd = new JTextField();
+	private JButton logBtn = new JButton();
+	private JButton regBtn = new JButton();
+
+	private Dimension userID_Size = new Dimension(230, 40);
+	private Dimension userPsd_Size = new Dimension(230, 40);
+	private Dimension logBtn_Size = new Dimension(90, 30);
+	private Dimension regBtn_size = new Dimension(90, 30);
+
+	private JPanel cPanel = new JPanel();
+	// private JPanel sPanel = new JPanel();
+	private JPanel nPanel = new JPanel();
+
+	private Dimension nPanel_Size = new Dimension(300, 260);
+	// private Dimension sPanel_Size = new Dimension(300, 100);
+
 	public void startUI() {
+
 		this.setSize(300, 500);
 		this.setTitle("ICQN START");
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 
+		userID.setText("userID");
+		userID.setPreferredSize(userID_Size);
+		userPsd.setText("userPsd");
+		userPsd.setPreferredSize(userPsd_Size);
+
+		cPanel.add(userID);
+		cPanel.add(userPsd);
+
+		logBtn.setText("login");
+		logBtn.setPreferredSize(logBtn_Size);
+		regBtn.setText("register");
+		regBtn.setPreferredSize(regBtn_size);
+
+		cPanel.add(logBtn);
+		cPanel.add(regBtn);
+
+		nPanel.setPreferredSize(nPanel_Size);
+		// sPanel.setPreferredSize(sPanel_Size);
+
+		this.add(nPanel, BorderLayout.NORTH);
+		this.add(cPanel, BorderLayout.CENTER);
+		// this.add(sPanel, BorderLayout.SOUTH);
+
 		this.setVisible(true);
+	}
+
+	public JTextField getUserID() {
+		return userID;
+	}
+
+	public JTextField getUserPsd() {
+		return userPsd;
 	}
 
 }
---
+++ src/StartUI/StartUITools.java
@@ -0,0 +1,30 @@
+package StartUI;
+
+import javax.swing.JTextField;
+
+public class StartUITools {
+
+	private StartUITools() {
+	}
+
+	public static byte[] userID_Burn(JTextField userID) {
+
+		byte[] userid;
+		String userid_1 = userID.getText();
+		userid = userid_1.getBytes();
+
+		return userid;
+
+	}
+
+	public static byte[] userPsd_Burn(JTextField userPsd) {
+
+		byte[] userpsd;
+		String userpsd_1 = userPsd.getText();
+		userpsd = userpsd_1.getBytes();
+
+		return userpsd;
+
+	}
+
+}
